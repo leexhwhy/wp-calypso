010240b14af7dfb0f21075984082d07a89be0555
Andrew Duthie <andrew@andrewduthie.com>
2016-08-02 15:03:05 -0400

    Framework: Update localStorage polyfill tests per new usage

--- client/lib/local-storage/test/index.js
+++ client/lib/local-storage/test/index.js
@@ -5,43 +5,41 @@ import { assert } from 'chai';
 
 describe( 'localStorage', function() {
 	describe( 'when window.localStorage does not exist', function() {
-		const window = {};
+		delete global.localStorage;
 
 		before( () => {
-			require( '..' )( window );
+			require( '..' );
 		} );
 
 		it( 'should create a window.localStorage instance', function() {
-			assert( window.localStorage );
+			assert( global.localStorage );
 		} );
 
 		it( 'should correctly store and retrieve data', function() {
-			window.localStorage.setItem( 'foo', 'bar' );
-			assert.equal( window.localStorage.getItem( 'foo' ), 'bar' );
-			assert.equal( window.localStorage.length, 1 );
+			global.localStorage.setItem( 'foo', 'bar' );
+			assert.equal( global.localStorage.getItem( 'foo' ), 'bar' );
+			assert.equal( global.localStorage.length, 1 );
 		} );
 	} );
 
 	describe( 'when window.localStorage is not working correctly', function() {
-		const window = {
-			localStorage: {}
-		};
+		global.localStorage = {};
 
 		before( () => {
-			require( '..' )( window );
+			require( '..' );
 		} );
 
 		it( 'should overwrite broken or missing methods', function() {
-			assert( window.localStorage.setItem );
-			assert( window.localStorage.getItem );
-			assert( window.localStorage.removeItem );
-			assert( window.localStorage.clear );
+			assert( global.localStorage.setItem );
+			assert( global.localStorage.getItem );
+			assert( global.localStorage.removeItem );
+			assert( global.localStorage.clear );
 		} );
 
 		it( 'should correctly store and retrieve data', function() {
-			window.localStorage.setItem( 'foo', 'bar' );
-			assert.equal( window.localStorage.getItem( 'foo' ), 'bar' );
-			assert.equal( window.localStorage.length, 1 );
+			global.localStorage.setItem( 'foo', 'bar' );
+			assert.equal( global.localStorage.getItem( 'foo' ), 'bar' );
+			assert.equal( global.localStorage.length, 1 );
 		} );
 	} );
 } );
