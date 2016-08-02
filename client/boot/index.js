1855b3d20293b71b6492171412b4072bc345a08c
Bernhard Reiter <ockham@raz.or.at>
2016-08-02 19:24:20 +0200

    DocumentHead: Set document.title via Redux middleware

--- client/boot/index.js
+++ client/boot/index.js
@@ -47,7 +47,6 @@ var config = require( 'config' ),
 	touchDetect = require( 'lib/touch-detect' ),
 	setRouteAction = require( 'state/ui/actions' ).setRoute,
 	accessibleFocus = require( 'lib/accessible-focus' ),
-	TitleStore = require( 'lib/screen-title/store' ),
 	syncHandler = require( 'lib/wp/sync-handler' ),
 	renderWithReduxStore = require( 'lib/react-helpers' ).renderWithReduxStore,
 	bindWpLocaleState = require( 'lib/wp/localization' ).bindState,
@@ -87,14 +86,6 @@ function init() {
 
 	// Add accessible-focus listener
 	accessibleFocus();
-
-	// Set document title
-	TitleStore.on( 'change', function() {
-		var title = TitleStore.getState().formattedTitle;
-		if ( title && title !== document.title ) {
-			document.title = title;
-		}
-	} );
 }
 
 function setUpContext( reduxStore ) {
--- client/state/document-head/middleware.js
+++ client/state/document-head/middleware.js
@@ -2,27 +2,80 @@
  * Internal dependencies
  */
 import { DOCUMENT_HEAD_TITLE_SET, DOCUMENT_HEAD_UNREAD_COUNT_SET } from 'state/action-types';
-import { setTitle as legacySetTitle, setCount as legacySetCount } from 'lib/screen-title/actions';
-import { getSelectedSiteId } from 'state/ui/selectors';
+import { decodeEntities } from 'lib/formatting';
+import { getTitle, getUnreadCount } from './selectors';
+import { getSelectedSite } from 'state/ui/selectors';
 
 /**
  * Middleware that updates the screen title when a title updating action is
- * dispatched. Currently, this dispatches through the legacy Flux actions used
- * elsewhere in the codebase.
+ * dispatched.
  *
  * @param {Object} store Redux store instance
  * @returns {Function} A configured middleware with store
  */
 export default ( { getState } ) => ( next ) => ( action ) => {
+	let formattedTitle;
 	switch ( action.type ) {
 		case DOCUMENT_HEAD_TITLE_SET:
-			legacySetTitle( action.title, { siteID: getSelectedSiteId( getState() ) } );
+			formattedTitle = getFormattedTitle(
+				action.title,
+				getUnreadCount( getState() ),
+				getSelectedSite( getState() )
+			);
+			if ( formattedTitle !== document.title ) {
+				document.title = formattedTitle;
+			}
 			break;
 
 		case DOCUMENT_HEAD_UNREAD_COUNT_SET:
-			legacySetCount( action.count, { siteID: getSelectedSiteId( getState() ) } );
+			formattedTitle = getFormattedTitle(
+				getTitle( getState() ),
+				action.count,
+				getSelectedSite( getState() )
+			);
+			if ( formattedTitle !== document.title ) {
+				document.title = formattedTitle;
+			}
 			break;
 	}
 
 	return next( action );
 };
+
+function getFormattedTitle( title, unreadCount, site ) {
+	let pageTitle = '';
+
+	if ( unreadCount ) {
+		pageTitle += '(' + unreadCount + ') ';
+	}
+
+	pageTitle += title;
+
+	if ( site ) {
+		pageTitle = appendSite( pageTitle, site );
+	}
+
+	if ( pageTitle ) {
+		pageTitle = decodeEntities( pageTitle ) + ' — WordPress.com';
+	} else {
+		pageTitle = 'WordPress.com';
+	}
+
+	return pageTitle;
+}
+
+function appendSite( title, site ) {
+	let siteName;
+
+	if ( site.name ) {
+		siteName = site.name;
+	} else {
+		siteName = site.URL.replace( /^https?:\/\//, '' ); // FIXME: site.domain computed attr
+	}
+
+	if ( title ) {
+		return title + ' \u2039 ' + siteName;
+	}
+
+	return siteName;
+}
